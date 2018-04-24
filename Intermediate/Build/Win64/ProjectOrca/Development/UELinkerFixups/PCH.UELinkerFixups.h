fa2adfc5c69ee71bc8ad851598850eac0df775c5
werethrope <werethrope@gmail.com>
2018-04-24 03:04:14 +0100

    Added character placeholder textures
    Added shark and animation preview of shark
    Changed character animations, no longer sliding
    Changed start level to James_LevelDemo
    Packaging will be tested here

---
+++ Intermediate/Build/Win64/ProjectOrca/Development/UELinkerFixups/PCH.UELinkerFixups.h
@@ -0,0 +1,2 @@
+// PCH for E:\Game Projects\projectorca\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
+#include "E:\Game Projects\projectorca\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
